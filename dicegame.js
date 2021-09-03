4399ab3532f5357acb6aba6793f352f64751a48c
Rafael masselli <89049153+rafaelmasselli@users.noreply.github.com>
2021-09-03 00:53:14 -0300

    Update dicegame.js

--- dicegame.js
+++ dicegame.js
@@ -1,23 +1,31 @@
 const prompt = require('prompt-sync')();
 /////////////////////////////////////////////
-//quantas vezes o usario vai jogar, quantas rodadas ira acontecer no jogo 
+
+// quantidade de vezes que o usario vai jogar, quantidade de playes no game
+
 let Rodadas = Number(prompt('    Digite quantas rodadas você deseja jogar?  '))
 const jogadores = [];
 const jgs = Number(prompt('    Digite quantos jogadores iram jogar  :'));
-// login name 
+
+// login name dos playes
+
 for (var i = 1; i <= jgs; i++) { 
     const jogador  = {};
 jogador.Nome = prompt(`    Digite o Nome do player ${i}  :`)    
 jogador.Resultados = [];
 jogadores.push(jogador);
 }
-// estrutura de rodads e numero aleatorio do random
+
+// estrutura de rodads e numeros aleatorio do random
+
 for (let Ordem in jogadores) { 
     for (let i = 0; i < Rodadas; i++) {  
     jogadores[Ordem].Resultados.push(Math.floor(Math.random() * (5 + 1) + 1));
  }
 }
+
 // estrutura de vitoria 
+
 jogadores.sort(function (a,b) {
     if (b.Resultados < a.Resultados){
         return -1;
@@ -32,5 +40,3 @@ for (let i = 0; i < jogadores.length; i++) {
 console.log('=============================================================')
 console.log(`        O vencedor e o jogador ${jogadores[0].Nome}              `)
 console.log('=============================================================')
-
-
